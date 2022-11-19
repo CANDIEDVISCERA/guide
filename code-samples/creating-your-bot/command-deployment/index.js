a3a56b3a6835e89488015008bc07bb4179868f50
CANDIEDVISCERA <50312195+CANDIEDVISCERA@users.noreply.github.com>
2022-11-19 12:36:41 -0800

    Update index.js

--- code-samples/creating-your-bot/command-deployment/index.js
+++ code-samples/creating-your-bot/command-deployment/index.js
@@ -19,19 +19,5 @@ client.once(Events.ClientReady, () => {
 	console.log('Ready!');
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
 
 client.login(token);
